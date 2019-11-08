1518001c32cca88ce9b30f272bd384ce4af3fb05
Andrew Gerdel <AndrewGerdel@yahoo.com>
2019-11-07 22:00:46 -0600

    mistake in unit test

--- Tests/sha256Tests.js
+++ Tests/sha256Tests.js
@@ -9,6 +9,6 @@ describe('sha256Tests', function() {
     it('should generate the same hash twice', async function() {
         var result = await sha256.CreateHash("Some Text");
         var result2 = await sha256.CreateHash("Some Text");
-        assert.equal(result.toString('hex'), result.toString('hex'), 'The hashes are different');
+        assert.equal(result.toString('hex'), result2.toString('hex'), 'The hashes are different');
     })
 });
