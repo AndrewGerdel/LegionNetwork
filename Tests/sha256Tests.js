073aa46bdf30bac15f2d9b48cdca1fd79a1555c5
Andrew Gerdel <AndrewGerdel@yahoo.com>
2019-11-07 22:18:20 -0600

    Sha256 unit tests

--- Tests/sha256Tests.js
+++ Tests/sha256Tests.js
@@ -10,5 +10,19 @@ describe('sha256Tests', function() {
         var result = await sha256.CreateHash("Some Text");
         var result2 = await sha256.CreateHash("Some Text");
         assert.equal(result.toString('hex'), result2.toString('hex'), 'The hashes are different');
-    })
+    }),
+    it('should generate the same hash for two objects', async function() {
+        var object = {Property1: "Value1", Property2: "Value2"};
+        var result = await sha256.CreateHash(JSON.stringify(object));
+        var object2 = {Property1: "Value1", Property2: "Value2"};
+        var result2 = await sha256.CreateHash(JSON.stringify(object2));
+        assert.equal(result.toString('hex'), result2.toString('hex'), 'The hashes are different');
+    }),
+    it('should generate different hashes for two object', async function() {
+        var object = {Property1: new Date(), Property2: "Value2"};
+        var result = await sha256.CreateHash(JSON.stringify(object));
+        var object2 = {Property1: new Date(), Property2: "Value3"};
+        var result2 = await sha256.CreateHash(JSON.stringify(object2));
+        assert.notEqual(result.toString('hex'), result2.toString('hex'), 'The hashes are different');
+    });
 });
